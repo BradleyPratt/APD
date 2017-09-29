9245b2bcc215e5b891930c3367573dddd5bdf211
B.J.Pratt U1664020 <U1664020@unimail.hud.ac.uk>
2017-09-29 15:03:45 +0100

    stuff

---
+++ Week1/week01/week01/src/intArrays/SimpleRandomListing.java
@@ -0,0 +1,48 @@
+package intArrays;
+
+import java.util.Arrays; // in order to be able to use the fill(...) method
+/**
+ * An extension of RandomCount
+ * 
+ * @author Hugh Osborne
+ * @version September 2017
+ */
+
+
+public class SimpleRandomListing extends RandomListing
+{
+    /**
+     * Constructor
+     */
+    public SimpleRandomListing(int size) {
+        super(size);
+    }
+    
+    /**
+     * Randomise the array
+     */
+    protected void randomise() {
+        int[] copy = new int[getArray().length];
+        int index = 0;
+        int randomIndex;
+        // used to indicate if elements have been used
+        boolean[] used = new boolean[getArray().length];
+        Arrays.fill(used,false);
+        for (; index < getArray().length; index++) {
+        	do {
+                randomIndex = getRandomIndex();
+            } while (used[randomIndex]);
+            copy[index] = array[randomIndex];
+            used[randomIndex] = true;
+        }
+        for (index = 0; index < getArray().length;) {
+            getArray()[index] = copy[index];
+        }
+    }
+    
+    public static void main(String[] args) {
+    	RandomListing count = new SimpleRandomListing(1000000);
+    	System.out.println(Arrays.toString(count.getArray()));
+    }
+
+} // End of class SimpleRandomCount
