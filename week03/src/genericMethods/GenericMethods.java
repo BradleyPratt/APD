f1921510860354a1ef4207adac69beb4887c7b88
Bradley Pratt <u1664020@unimail.hud.ac.uk>
2017-10-25 17:37:45 +0100

    Week4

--- week03/src/genericMethods/GenericMethods.java
+++ week03/src/genericMethods/GenericMethods.java
@@ -1,6 +1,7 @@
 package genericMethods;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class GenericMethods {	
 	/**
@@ -25,11 +26,28 @@ public class GenericMethods {
 		array[index2] = objectOne; //Uses the defined second position and places "objectOne" there
 	}
 	
+    public static <T> String max(String[] array, int index1, int index2) {
+    	int index = 0;
+    	int elementLength = array[0].length();
+    	System.out.println();
+    	for (int i = 0; i < array.length; i++) {
+    		if (i >= index1 && i <= index2) {
+    			if (array[i].length() > elementLength) {
+    				index = i;
+    				elementLength = array[i].length();
+    			}
+    		}
+    	}
+    	return array[index];
+    }
+	
     public static String main() {
     	Integer[] arrayTest = {1,2,3,4,5};
     	swap(arrayTest, 1, 2); //Chooses which objects from the defined index location to swap
     	System.out.println(Arrays.toString(arrayTest));
-    	
+    	String[] names = {"Hugh", "Andrew", "Ebrahim","Diane","Paula", "Simon"};
+    	System.out.println(max(names,1,4));
     	return Arrays.toString(arrayTest);
     }
+    
 }
