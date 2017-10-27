ea0643b4c3ec26dfb8ea1f5169b1d33bb92a1789
Bradley Pratt <u1664020@unimail.hud.ac.uk>
2017-10-27 21:53:43 +0100

    Update

---
+++ week05/src/arraySorter/QuickSort.java
@@ -0,0 +1,14 @@
+package arraySorter;
+
+import java.util.Comparator;
+
+/**
+ * Quick sort class to sort the array using a quick sort
+ * 
+ * @author Bradley Pratt
+ */
+public class QuickSort<T extends Comparable<? super T>> extends ArraySortTool<T> {
+	public void sort(T[] array) {
+
+	}
+}
--- week05/src/arraySorter/SelectionSort.java
+++ week05/src/arraySorter/SelectionSort.java
@@ -1,18 +1,45 @@
 package arraySorter;
 
+import java.util.Arrays;
+
 /**
  * Selection sort class, sorts the array using selection sort
  * 
  * @author Bradley Pratt
  */
 public class SelectionSort<T extends Comparable<? super T>> extends ArraySortTool<T> {
+	
 	public void sort(T[] array) {
-		for (int lastUnsorted = array.length-1; lastUnsorted > 0; lastUnsorted--) {
-			for (int nextToCompare = 0; nextToCompare < lastUnsorted; nextToCompare++) {
-				if (nextToCompare) {
-					
-				}
-			}
+    	for (int i = 0; i < array.length; i++) {
+    		int minIndex = i;
+    		for (int j = i + 1; j < array.length; j++) {
+    			if (array[j].compareTo(array[minIndex]) < 0) {
+    				minIndex = j;
+    			}
+    		}
+    		if (minIndex != i) {
+    			T temp = array[i];
+    			array[i] = array[minIndex];
+    			array[minIndex] = temp;
+    		}
 		}
+	}
+
+
+public static void main(String[] args){
+    SelectionSort firstsort = new SelectionSort();
+
+    Integer[] arr = {3,4,1,5};
+    System.out.println("before sorting int: "+ Arrays.toString(arr));
+    firstsort.sort(arr);
+    System.out.println("After sorting int : "+Arrays.toString(arr));
+     String[] arr1= {"acd","ded","dal","bad","cle"};
+     System.out.println("before sorting String: "+ Arrays.toString(arr1));
+     firstsort.sort(arr1);
+     System.out.println("After sorting String : "+Arrays.toString(arr1));
+     Character[] arr2= {'c','e','a','d','c'};
+     System.out.println("before sorting char: "+ Arrays.toString(arr2));
+     firstsort.sort(arr2);
+     System.out.println("After sorting char : "+Arrays.toString(arr2));
 	}
 }
