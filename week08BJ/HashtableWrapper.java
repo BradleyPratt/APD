280ed7b944942382ecee45984a711e4f6dc5919f
Bradley Pratt <u1664020@unimail.hud.ac.uk>
2017-11-17 16:09:16 +0000

    Week8

---
+++ week08BJ/HashtableWrapper.java
@@ -0,0 +1,14 @@
+ 
+
+/**
+ * A wrapper for the Hashtable class
+ * 
+ * @author Hugh Osborne 
+ * @version October 2011
+ */
+public class HashtableWrapper<S,T> extends java.util.Hashtable<S,T>
+{
+    public HashtableWrapper(int size) {
+        super(size);
+    }
+}
